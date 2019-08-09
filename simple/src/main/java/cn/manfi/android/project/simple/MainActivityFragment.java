44abae379332dd6ef494252b63058a726f692164
Manfi <a343460557@aliyun.com>
2019-08-09 10:59:10 +0800

    更新项目到AndroidX； 更新第三方库版本；

--- simple/src/main/java/cn/manfi/android/project/simple/MainActivityFragment.java
+++ simple/src/main/java/cn/manfi/android/project/simple/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package cn.manfi.android.project.simple;
 
-import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,5 +21,13 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.vg).setOnClickListener(v -> {
+
+        });
     }
 }
